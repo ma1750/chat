bd8ac6cd64259ec6865979d8e488813716ee1cc1
ma1750 <mc.ma1750@gmail.com>
2021-05-12 19:48:47 +0900

    client

--- client.c
+++ client.c
@@ -5,8 +5,12 @@
 #include <string.h>
 #include <netinet/in.h>
 #include <netdb.h>
+#include <pthread.h>
 
 #define BUF_LEN 128
+
+int input(char*);
+void *listen_func(int);
 
 int main(int argc, char const *argv[])
 {
@@ -15,7 +19,7 @@ int main(int argc, char const *argv[])
         exit(EXIT_FAILURE);
     }
 
-    char *server_addr = argv[1];
+    const char *server_addr = argv[1];
     int port = atoi(argv[2]);
     if (!port) {
         fprintf(stderr, "server port is invalid\n");
@@ -39,7 +43,10 @@ int main(int argc, char const *argv[])
     if (ret == -1) {
         perror("getaddrinfo() failed");
         freeaddrinfo(ai);
-        close(socketfd);
+        if (close(socketfd) == -1) {
+            perror("close failed");
+            exit(EXIT_FAILURE);
+        }
         exit(EXIT_FAILURE);
     }
 
@@ -51,11 +58,106 @@ int main(int argc, char const *argv[])
     ret = connect(socketfd, (struct sockaddr *)&server, sizeof(server));
     if (ret == -1) {
         perror("connect() failed");
-        close(socketfd);
+        if (close(socketfd) == -1) {
+            perror("close failed");
+            exit(EXIT_FAILURE);
+        }
         exit(EXIT_FAILURE);
     }
 
+    char recv_buf[BUF_LEN];
+    int recv_len;
+    if ((recv_len = recv(socketfd, recv_buf, BUF_LEN, 0)) == -1) {
+        perror("recv failed");
+        if (close(socketfd) == -1) {
+            perror("close failed");
+            exit(EXIT_FAILURE);
+        }
+        exit(EXIT_FAILURE);
+    }
+    if (strncmp(recv_buf, "connect OK", recv_len) != 0) {
+        fprintf(stderr, "invalid packet: %s\n", recv_buf);
+        if (close(socketfd) == -1) {
+            perror("close failed");
+            exit(EXIT_FAILURE);
+        }
+        exit(EXIT_FAILURE);
+    }
 
-    close(socketfd);
+    printf("Input your name. Up to 20 characters >");
+    char input_buf[BUF_LEN];
+    int input_len;
+    if ((input_len = input(&input_buf)) == -1) {
+        // error
+        fprintf(stderr, "invalid input");
+        if (close(socketfd) == -1) {
+            perror("close failed");
+            exit(EXIT_FAILURE);
+        }
+        exit(EXIT_FAILURE);
+    }
+
+    if (input_len > 20)
+        input_len = 20;
+
+    send(socketfd, input_buf, input_len, NULL);
+
+    pthread_t listen_thread;
+    pthread_create(&listen_thread, NULL, (void *)listen_func, NULL);
+
+    do {
+        if ((input_len = input(input_buf)) == -1) {
+            fprintf(stderr, "invalid input");
+            break;
+        }
+        send(socketfd, input_buf, input_len, 0);
+    } while (strncmp(input_buf, "exit", 4) != 0);
+
+    if (listen_thread != NULL) {
+        if ((pthread_join(listen_thread, NULL) != 0)){
+            perror("pthread_join failed");
+            if (close(socketfd) == -1) {
+                perror("close failed");
+                exit(EXIT_FAILURE);
+            }
+        }
+        listen_thread = NULL;
+    }
+
+    if (close(socketfd) == -1) {
+        perror("close failed");
+        exit(EXIT_FAILURE);
+    }
     return 0;
+}
+
+
+int input(char *ret_ptr)
+{
+    int ret_len;
+    while (fgets(ret_ptr, BUF_LEN, stdin) == NULL || ret_ptr[0] == '\n') {
+        fprintf(stderr, "fgets error");
+    }
+
+    ret_len = strlen(ret_ptr);
+    if (ret_ptr[ret_len-1] == '\n')
+        ret_ptr[--ret_len] = '\0';
+
+    return ret_len;
+}
+
+
+void *listen_func(int _socketfd)
+{
+    char recv_buf[BUF_LEN];
+    int recv_len;
+    do {
+        if ((recv_len = recv(_socketfd, recv_buf, BUF_LEN, 0)) == -1) {
+            fprintf(stderr, "[tid: %ld]recv failed", pthread_self());
+            perror(NULL);
+            break;
+        }
+    } while (strncmp(recv_buf, "exit_ack", 8) == 0);
+
+    return NULL;
 }
